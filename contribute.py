eaf3f411806d6102ed3cb0eff9afc6669404454e
zayncodes <askzayn9@gmail.com>
2024-11-30 02:51:51 +0530

    Automated daily commit

--- contribute.py
+++ contribute.py
@@ -1,128 +1,76 @@
-#!/usr/bin/env python
-import argparse
 import os
-from datetime import datetime
-from datetime import timedelta
-from random import randint
-from subprocess import Popen
+import subprocess
+import time
+import random
 import sys
-
-
-def main(def_args=sys.argv[1:]):
-    args = arguments(def_args)
-    curr_date = datetime.now()
-    directory = 'repository-' + curr_date.strftime('%Y-%m-%d-%H-%M-%S')
-    repository = args.repository
-    user_name = args.user_name
-    user_email = args.user_email
-    if repository is not None:
-        start = repository.rfind('/') + 1
-        end = repository.rfind('.')
-        directory = repository[start:end]
-    no_weekends = args.no_weekends
-    frequency = args.frequency
-    days_before = args.days_before
-    if days_before < 0:
-        sys.exit('days_before must not be negative')
-    days_after = args.days_after
-    if days_after < 0:
-        sys.exit('days_after must not be negative')
-    os.mkdir(directory)
-    os.chdir(directory)
-    run(['git', 'init', '-b', 'main'])
-
-    if user_name is not None:
-        run(['git', 'config', 'user.name', user_name])
-
-    if user_email is not None:
-        run(['git', 'config', 'user.email', user_email])
-
-    start_date = curr_date.replace(hour=20, minute=0) - timedelta(days_before)
-    for day in (start_date + timedelta(n) for n
-                in range(days_before + days_after)):
-        if (not no_weekends or day.weekday() < 5) \
-                and randint(0, 100) < frequency:
-            for commit_time in (day + timedelta(minutes=m)
-                                for m in range(contributions_per_day(args))):
-                contribute(commit_time)
-
-    if repository is not None:
-        run(['git', 'remote', 'add', 'origin', repository])
-        run(['git', 'branch', '-M', 'main'])
-        run(['git', 'push', '-u', 'origin', 'main'])
-
-    print('\nRepository generation ' +
-          '\x1b[6;30;42mcompleted successfully\x1b[0m!')
-
-
-def contribute(date):
-    with open(os.path.join(os.getcwd(), 'README.md'), 'a') as file:
-        file.write(message(date) + '\n\n')
-    run(['git', 'add', '.'])
-    run(['git', 'commit', '-m', '"%s"' % message(date),
-         '--date', date.strftime('"%Y-%m-%d %H:%M:%S"')])
-
-
-def run(commands):
-    Popen(commands).wait()
-
+import argparse
+from datetime import datetime, timedelta
 
 def message(date):
-    return date.strftime('Contribution: %Y-%m-%d %H:%M')
+    """Generate commit message"""
+    return f"Contribution: {date.strftime('%Y-%m-%d %H:%M')}"
 
+def run(commands):
+    """Execute a command and wait for it to finish"""
+    print(f"Running command: {commands}")
+    try:
+        process = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate(timeout=180)  # Increased timeout to 180 seconds
+        if stdout:
+            print(stdout.decode())
+        if stderr:
+            print(stderr.decode())
+        process.wait()
+    except subprocess.TimeoutExpired:
+        print("Timeout expired. The command took too long to execute.")
+    except Exception as e:
+        print(f"Error executing command: {e}")
 
-def contributions_per_day(args):
-    max_c = args.max_commits
-    if max_c > 20:
-        max_c = 20
-    if max_c < 1:
-        max_c = 1
-    return randint(1, max_c)
+def contribute(date):
+    """Generate commit and add changes to the repo"""
+    print(f"Committing for date: {date}")
+    with open(os.path.join(os.getcwd(), 'README.md'), 'a') as file:
+        file.write(message(date) + '\n\n')
 
+    print("Running git add...")
+    run(['git', 'add', '--all'])  # Use '--all' to add new and modified files
 
-def arguments(argsval):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-nw', '--no_weekends',
-                        required=False, action='store_true', default=False,
-                        help="""do not commit on weekends""")
-    parser.add_argument('-mc', '--max_commits', type=int, default=10,
-                        required=False, help="""Defines the maximum amount of
-                        commits a day the script can make. Accepts a number
-                        from 1 to 20. If N is specified the script commits
-                        from 1 to N times a day. The exact number of commits
-                        is defined randomly for each day. The default value
-                        is 10.""")
-    parser.add_argument('-fr', '--frequency', type=int, default=80,
-                        required=False, help="""Percentage of days when the
-                        script performs commits. If N is specified, the script
-                        will commit N%% of days in a year. The default value
-                        is 80.""")
-    parser.add_argument('-r', '--repository', type=str, required=False,
-                        help="""A link on an empty non-initialized remote git
-                        repository. If specified, the script pushes the changes
-                        to the repository. The link is accepted in SSH or HTTPS
-                        format. For example: git@github.com:user/repo.git or
-                        https://github.com/user/repo.git""")
-    parser.add_argument('-un', '--user_name', type=str, required=False,
-                        help="""Overrides user.name git config.
-                        If not specified, the global config is used.""")
-    parser.add_argument('-ue', '--user_email', type=str, required=False,
-                        help="""Overrides user.email git config.
-                        If not specified, the global config is used.""")
-    parser.add_argument('-db', '--days_before', type=int, default=365,
-                        required=False, help="""Specifies the number of days
-                        before the current date when the script will start
-                        adding commits. For example: if it is set to 30 the
-                        first commit date will be the current date minus 30
-                        days.""")
-    parser.add_argument('-da', '--days_after', type=int, default=0,
-                        required=False, help="""Specifies the number of days
-                        after the current date until which the script will be
-                        adding commits. For example: if it is set to 30 the
-                        last commit will be on a future date which is the
-                        current date plus 30 days.""")
-    return parser.parse_args(argsval)
+    print("Running git commit...")
+    run(['git', 'commit', '-m', message(date), '--date', date.strftime('%Y-%m-%d %H:%M:%S'), '--no-verify', '--quiet', '--allow-empty'])
 
+    print(f"Commit completed for {date}")
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Automate git commits")
+    parser.add_argument('--frequency', type=int, default=80, help="Frequency of commits (in seconds)")
+    parser.add_argument('--max_commits', type=int, default=17, help="Maximum number of commits")
+    parser.add_argument('--repository', type=str, required=True, help="URL of the GitHub repository")
+    args = parser.parse_args()
+
+    # Extract repository name from URL (get folder name from the URL)
+    repo_name = args.repository.split('/')[-1].replace('.git', '')
+
+    # Clone the repository if it doesn't exist
+    if not os.path.exists(repo_name):
+        print(f"Cloning repository: {args.repository}")
+        run(['git', 'clone', args.repository])
+
+    os.chdir(repo_name)  # Change to the repository directory
+
+    # Start contributing
+    current_time = datetime.now()
+    commits_made = 0
+    while commits_made < args.max_commits:
+        contribute(current_time)  # Make a commit for the current time
+        commits_made += 1
+        print(f"Commit {commits_made}/{args.max_commits} made at {current_time}")
+
+        # Sleep for the desired frequency before making the next commit
+        current_time += timedelta(seconds=args.frequency)
+
+        # Random sleep time to simulate natural intervals
+        time.sleep(random.uniform(0.5, 1.5))
 
 if __name__ == "__main__":
     main()
